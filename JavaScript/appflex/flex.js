65c73182206b09fa8668294d1a172394373cb482
Pedro Sarmento <104094302+pesarmento@users.noreply.github.com>
2023-11-07 16:33:49 -0300

    Add files via upload

---
+++ JavaScript/appflex/flex.js
@@ -0,0 +1,19 @@
+/**
+ * App Flex
+ * @author Pedro Sarmento
+ * https://github.com/pesarmento
+ */
+
+function calcular(){
+    let etanol = frmFlex.txtEtanol.value
+    let gasolina = frmFlex.txtGasolina.value
+    if(etanol < 0.7 * gasolina) {
+        document.getElementById('status').src='icons/etanol.png'
+    } else {
+        document.getElementById('status').src='icons/gasolina.png'
+    }
+}
+
+function limpar(){
+    document.getElementById('status').src='icons/neutro.png'
+}
